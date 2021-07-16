8c5a7447f1ac2e56657b4853bbcf55d8b80b2bc0
Ramy Huffman <rah5757@psu.edu>
2021-07-16 11:02:43 -0700

    Update Index File
    index.js file can read static URL image link and render a long consecutive string of artist names.

--- lambda-function/index.js
+++ lambda-function/index.js
@@ -1,8 +1,7 @@
+const fetch = require('node-fetch');
+
 exports.handler = async (event) => {
-    const message = {
-        statusCode: 200,
-        body: JSON.stringify('A GET request has been made from Lambda')
-    }
-    
-    return message;
+  const res = await fetch('https://api.ocr.space/parse/imageurl?apikey=b4182a4ab988957&url=https://festuff-production.s3.amazonaws.com/uploads/image/attachment/42789/lineup-666-poster-5a273000-491b-421a-8755-3dc0a03034ff.jpg');
+  const json = await res.json();
+  return json.ParsedResults.pop().ParsedText.replace(/(\r\n|\n|\r)/gm, "");
 };
