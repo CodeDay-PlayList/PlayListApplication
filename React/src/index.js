814354e572efb8c7f58590070e10d0a9bb88e0d1
Robinson Ibarra <ribarra764@gmail.com>
2021-07-18 18:58:25 -0700

    Select file updated
    Removed Select file button and opted to use screen as tap to select.

--- React/src/index.js
+++ React/src/index.js
@@ -1,33 +1,47 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 import { CameraOutline } from "react-ionicons";
 
 function ImageUpload() {
-  // state = {
-  //   selectedFile: null,
-  // };
+  const inputFileRef = useRef(null);
 
-  // const fileSelectedHandler = (event) => {
-  //   this.setState({
-  //     selectedFile: event.target.files[0],
-  //   });
-  // };
+  // let fileName;
 
-  // const fileUploadHandler = () => {};
+  const onFileChange = (e) => {
+    // fileName = `Selected File - ${e.target.files[0].name}`;
+    console.log(`Selected File - ${e.target.files[0].name}`);
+  };
+
+  const onBtnClick = () => {
+    inputFileRef.current.click();
+    setActive(!isActive);
+  };
+
+  const [isActive, setActive] = useState("false");
+
   return (
     <div className="imageupload">
       <header className="title">
         <h2>PlayFest</h2>
       </header>
-      <div className="tapupload">
+      <div className={isActive ? "inactive" : "tapuploadfile"}>
+        <label htmlFor="file" onChange={onFileChange}>
+          fileName
+        </label>
+      </div>
+      <div className={isActive ? "tapupload" : "inactive"} onClick={onBtnClick}>
         <CameraOutline color={"#00000"} height="100px" width="100px" />
         <p>Take a Picture Or Browse your library to Upload a photo</p>
       </div>
       <div className="uploadbutton">
-        <input style={{ display: "none" }} type="file" />
-        <button>Select</button>
-        <button style={{ background: "#22c1c3" }}>Upload</button>
+        <input
+          style={{ display: "none" }}
+          type="file"
+          ref={inputFileRef}
+          onChange={onFileChange}
+        />
+        <button>Upload</button>
       </div>
     </div>
   );
