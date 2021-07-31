f01b79b1753acbd93a876b37c644464611db2389
Robinson Ibarra <ribarra764@gmail.com>
2021-07-31 12:14:42 -0700

    Added OCR to front-end
    Added Tesseract.js to front-end.

--- React/src/index.js
+++ React/src/index.js
@@ -1,15 +1,19 @@
 import React, { useRef, useState } from "react";
+import axios from "axios";
 import ReactDom from "react-dom";
 import "./index.css";
 import { CameraOutline } from "react-ionicons";
+import Tesseract from "tesseract.js";
 
 function ImageUpload() {
   const inputFileRef = useRef(null);
   const [isActive, setActive] = useState("false");
   const [previewChange, previewSet] = useState("image");
+  const [fufilledImage, currentImg] = useState(null);
 
   const onFileChange = (e) => {
     previewSet(URL.createObjectURL(e.target.files[0]));
+    currentImg(artistStrip(e.target.files[0]));
     console.log(`Selected File - ${e.target.files[0].name}`);
   };
 
@@ -18,13 +22,35 @@ function ImageUpload() {
     setActive(!isActive);
   };
 
+  const artistStrip = async (img) => {
+    Tesseract.recognize(img, "eng", {
+      logger: (m) => console.log(m),
+    }).then(({ data: { words } }) => {
+      console.log(`Hello ${words}`);
+      return words;
+    });
+  };
+
+  const uploadHandler = () => {
+    // const fd = new FormData();
+    // fd.append("image", fufilledImage);
+    axios
+      .post(
+        "https://88q3as42oj.execute-api.us-east-1.amazonaws.com/testPlay/GoogleVision",
+        fufilledImage
+      )
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
     <div className="imageupload">
       <header className="title">
         <h2>PlayFest</h2>
       </header>
       <div className={isActive ? "inactive" : "tapuploadfile"}>
-        <img src={previewChange} alt="image" />
+        <img src={previewChange} alt="Music Poster" />
       </div>
       <div className={isActive ? "tapupload" : "inactive"} onClick={onBtnClick}>
         <CameraOutline color={"#00000"} height="100px" width="100px" />
@@ -37,7 +63,7 @@ function ImageUpload() {
           ref={inputFileRef}
           onChange={onFileChange}
         />
-        <button>Upload</button>
+        <button onClick={uploadHandler}>Upload</button>
       </div>
     </div>
   );
--- lambda-function/index.js
+++ lambda-function/index.js
@@ -20,9 +20,9 @@ async function contentVision() {
 // contentVision();
 
 exports.handler = async (event) => {
-  const fetch = require('node-fetch');
-  const fuzzy = require('fuse.js');
-  const fs = require('fs');
+  const fetch = require("node-fetch");
+  const fuzzy = require("fuse.js");
+  const fs = require("fs");
 
   const artists = await contentVision();
   // const artists = "Hello";
@@ -43,60 +43,43 @@ exports.handler = async (event) => {
 
   //return response;
 
-  const res = await fetch('https://api.ocr.space/parse/imageurl?apikey=b4182a4ab988957&url=https://festuff-production.s3.amazonaws.com/uploads/image/attachment/42789/lineup-666-poster-5a273000-491b-421a-8755-3dc0a03034ff.jpg');
+  const res = await fetch(
+    "https://api.ocr.space/parse/imageurl?apikey=b4182a4ab988957&url=https://festuff-production.s3.amazonaws.com/uploads/image/attachment/42789/lineup-666-poster-5a273000-491b-421a-8755-3dc0a03034ff.jpg"
+  );
   const json = await res.json();
   const textArray = json.ParsedResults.pop()
     .ParsedText.toLowerCase()
-    .replace(/the/g, '')
-    .replace(/and/g, '')
-    .replace(/its/g, '')
+    .replace(/the/g, "")
+    .replace(/and/g, "")
+    .replace(/its/g, "")
     .replace(/(\r\n|\n|\r)/gm, " ")
     .replace(/[^\w\s]|_/g, "")
-    .split(' ')
+    .split(" ")
     .filter((item) => item.length > 2);
-    
-  const database = fs.readFileSync('ArtistList.txt', 'utf8').replace(/(\r)/gm, " ").split('\n').map(x=>x.trim());
+
+  const database = fs
+    .readFileSync("ArtistList.txt", "utf8")
+    .replace(/(\r)/gm, " ")
+    .split("\n")
+    .map((x) => x.trim());
 
   let validArtists = [];
-  
+
   const options = {
     includeScore: true,
-    threshold: 0.25
-  }
-  
+    threshold: 0.25,
+  };
+
   const fuse = new fuzzy(database, options);
-  
+
   for (let word of textArray) {
-    const result = fuse.search(word).sort((a,b) => a.score - b.score)[0];
+    const result = fuse.search(word).sort((a, b) => a.score - b.score)[0];
     if (result) {
-      if(!validArtists.includes(database[result.item])) {
-        validArtists.push(database[result.item])
+      if (!validArtists.includes(database[result.item])) {
+        validArtists.push(database[result.item]);
       }
     }
   }
 
   return validArtists;
 };
-
-//////////////////////////////////////////////////
-// const vision = require("@google-cloud/vision");
-
-// Code for converting to base64
-// const fs = require("fs");
-// const imageFile = fs.readFileSync("./OL2.jpg");
-// let encoded = Buffer.from(imageFile).toString("base64");
-
-// client
-//   .textDetection("./OL2.jpg")
-//   .then((results) => {
-//     const text = results[0].textAnnotations;
-
-//     console.log("Text:");
-//     text.forEach((text) => console.log(text.description));
-//   })
-//   .catch((err) => {
-//     console.error("ERROR:", err);
-//   });
-//   let hello = JSON.stringify(event);
-//   console.log(hello);
-// };
