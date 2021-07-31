418295564ac886d99ae6eb88f45b0469a2a285f6
Ramy Huffman <rah5757@psu.edu>
2021-07-31 10:57:04 -0700

    Update Spotify API Logic
    Creates new playlist programmatically and opens window to the playlist.

--- spotify-test/index.js
+++ spotify-test/index.js
@@ -1,18 +1,29 @@
-var SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyWebApi = require('spotify-web-api-node');
+const open = require('open');
 
 // credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId: 'ENTER ID',
-    clientSecret: 'ENTER SECRET',
-    redirectUri: 'ENTER URI'
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'CLIENT ID',
+    clientSecret: 'CLIENT SECRET',
+    redirectUri: 'REDIRECT URI'
 });
 
-spotifyApi.setAccessToken('ENTER TOKEN HERE');
+spotifyApi.setAccessToken('ACCESS TOKEN');
 
 let tracks = '';
+let playlistID = '';
 
 const artists = [
-    'Tame Impala', 'Lizzo', 'The Strokes'];
+    'Tame Impala', 'Lizzo', 'The Strokes'
+];
+
+// Create a private playlist
+spotifyApi.createPlaylist('CodeDay Demo Playlist', { 'description': 'My description', 'public': true })
+    .then(function(data) {
+        playlistID = (data.body.id).toString();
+    }, function(err) {
+        console.log('Something went wrong!', err);
+    });
 
 for (let i = 0; i < artists.length; i++) {
     spotifyApi.searchArtists(artists[i]).then(
@@ -20,12 +31,11 @@ for (let i = 0; i < artists.length; i++) {
             spotifyApi.getArtistTopTracks((data.body.artists.items.sort((a,b) => (a.popularity > b.popularity))[0].id), 'US').then (
                 function(data) {
                     tracks = (data.body.tracks[0].uri).toString();
-
                     // Add tracks to a playlist
-                    spotifyApi.addTracksToPlaylist('ENTER PLAYLIST ID HERE', [tracks])
+                    spotifyApi.addTracksToPlaylist(playlistID, [tracks])
                         .then(function(data) {
                             if (i === artists.length - 1) {
-                                console.log('Check out the new playlist... https://open.spotify.com/playlist/ENTER PLAYLIST ID');
+                                open(`https://open.spotify.com/playlist/${playlistID}`);
                             }
                         }, function(err) {
                             console.log('Something went wrong!', err);
