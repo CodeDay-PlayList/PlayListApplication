332ccec87e2dffd5e4df941d4dd8ad0fee100c8c
Robinson Ibarra <ribarra764@gmail.com>
2021-08-03 13:37:24 -0700

    Update Lambda Function
    Lambda function receives array from font-end and performs some manipulation with fuzzy search, however there is still some errors with fuzzy search that need resolving.

--- lambda-function/index.js
+++ lambda-function/index.js
@@ -1,34 +1,50 @@
 ("use strict");
+const fuzzy = require("fuse.js");
+const fs = require("fs");
 
-async function contentVision() {
-  const vision = require("@google-cloud/vision");
+const database = fs
+  .readFileSync("ArtistList.txt", "utf8")
+  .replace(/(\r)/gm, " ")
+  .split("\n")
+  .map((x) => x.trim());
 
-  const client = new vision.ImageAnnotatorClient({
-    keyFilename: "./GCVOCRkey.json",
-  });
-  const fileName = "./OL2.jpg";
+const options = {
+  includeScore: true,
+  threshold: 0.25,
+};
 
-  // Performs text detection on the local file
-  const [result] = await client.textDetection(fileName);
-  const detections = result.textAnnotations;
-  let artists = [];
-  detections.forEach((text) => artists.push(text.description));
-  // console.log(artists);
-  return JSON.stringify(artists);
-}
+const fuse = new fuzzy(database, options);
 
-// contentVision();
-
+// AWS Lambda Handler
 exports.handler = async (event) => {
-  const fetch = require("node-fetch");
-  const fuzzy = require("fuse.js");
-  const fs = require("fs");
+  let validArtists = [];
 
-  const artists = await contentVision();
-  // const artists = "Hello";
+  let artists;
+  artists = event.body;
+  console.log(
+    "Current Artists: " + artists,
+    "Event: " + event,
+    "Valid Artists" + validArtists
+  );
+
+  // for (let word of artists) {
+  //   const result = fuse.search(word).sort((a, b) => a.score - b.score)[0];
+  //   if (result) {
+  //     if (!validArtists.includes(database[result.item])) {
+  //       validArtists.push(database[result.item]);
+  //     }
+  //   }
+  // }
 
   let response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers":
+        "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+    },
     body: artists,
     isBase64Encoded: false,
   };
@@ -36,50 +52,16 @@ exports.handler = async (event) => {
   if (!artists) {
     response = {
       statusCode: 400,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
       body: "Something went wrong",
       isBase64Encoded: false,
     };
   }
 
-  //return response;
-
-  const res = await fetch(
-    "https://api.ocr.space/parse/imageurl?apikey=b4182a4ab988957&url=https://festuff-production.s3.amazonaws.com/uploads/image/attachment/42789/lineup-666-poster-5a273000-491b-421a-8755-3dc0a03034ff.jpg"
-  );
-  const json = await res.json();
-  const textArray = json.ParsedResults.pop()
-    .ParsedText.toLowerCase()
-    .replace(/the/g, "")
-    .replace(/and/g, "")
-    .replace(/its/g, "")
-    .replace(/(\r\n|\n|\r)/gm, " ")
-    .replace(/[^\w\s]|_/g, "")
-    .split(" ")
-    .filter((item) => item.length > 2);
-
-  const database = fs
-    .readFileSync("ArtistList.txt", "utf8")
-    .replace(/(\r)/gm, " ")
-    .split("\n")
-    .map((x) => x.trim());
-
-  let validArtists = [];
-
-  const options = {
-    includeScore: true,
-    threshold: 0.25,
-  };
-
-  const fuse = new fuzzy(database, options);
-
-  for (let word of textArray) {
-    const result = fuse.search(word).sort((a, b) => a.score - b.score)[0];
-    if (result) {
-      if (!validArtists.includes(database[result.item])) {
-        validArtists.push(database[result.item]);
-      }
-    }
-  }
-
-  return validArtists;
+  return response;
 };
