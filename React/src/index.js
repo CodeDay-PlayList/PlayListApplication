c8f7da2a5cd92ed7cb2facee0e1cd2719fcc3216
Robinson Ibarra <ribarra764@gmail.com>
2021-08-03 13:48:27 -0700

    Update front-end file
    Removed non-essential code that didn't work with current implementation and added comments for better readability,

--- React/src/index.js
+++ React/src/index.js
@@ -4,53 +4,27 @@ import ReactDom from "react-dom";
 import "./index.css";
 import { CameraOutline } from "react-ionicons";
 import Tesseract from "tesseract.js";
-// import myText from "./ArtistList.txt";
-// const fetch = require("node-fetch");
 
 function ImageUpload() {
   const inputFileRef = useRef(null);
   const [isActive, setActive] = useState("false");
   const [previewChange, previewSet] = useState("image");
   const [fufilledImage, currentImg] = useState([]);
-  // const [database, setDatabase] = useState([]);
-
-  const onFileChange = (e) => {
-    previewSet(URL.createObjectURL(e.target.files[0]));
-    artistStrip(e.target.files[0]);
-    console.log(`Selected File - ${e.target.files[0].name}`);
-  };
 
   const onBtnClick = () => {
     inputFileRef.current.click();
     setActive(!isActive);
   };
 
-  // const artistStrip = async (img) => {
-  //   Tesseract.recognize(img, "eng", {
-  //     logger: (m) => console.log(m),
-  //   }).then(({ data: { words } }) => {
-  //     let finResults = [];
-  //     let results = words.filter((word) => word.confidence >= 80);
-  //     results.forEach((word) => finResults.push(word.text));
-  //     console.log(words);
-  //     console.log(finResults);
-  //     currentImg(finResults);
-  //   });
-  // };
+  // Function that sets image uploaded by user and calls artistStrip function
+  const onFileChange = (e) => {
+    previewSet(URL.createObjectURL(e.target.files[0]));
+    artistStrip(e.target.files[0]);
+    console.log(`Selected File - ${e.target.files[0].name}`);
+  };
 
-  // const dbText = () => {
-  //   setDatabase(
-  //     fetch(myText)
-  //       .then((r) => r.text())
-  //       .then((text) => {
-  //         text
-  //           .replace(/(\r)/gm, " ")
-  //           .split("\n")
-  //           .map((x) => x.trim());
-  //       })
-  //   );
-  // };
-
+  // Function that uses Tesseract OCR & creates an array
+  // from artist names before sending to post
   const artistStrip = async (img) => {
     Tesseract.recognize(img, "eng", {
       logger: (m) => console.log(m),
@@ -64,22 +38,14 @@ function ImageUpload() {
         .replace(/[^\w\s]|_/g, "")
         .split(" ")
         .filter((item) => item.length > 2);
-      // dbText();
-      // console.log(database);
-      // let reader = new FileReader();
-      // reader.readAsText(myText);
-      // reader.onload = () => {
-      //   console.log(reader.result);
-      // };
-
       console.log(manipulateText);
       currentImg(manipulateText);
     });
   };
 
+  // Function sends POST request of an array
+  // of artists to Lambda function
   const uploadHandler = () => {
-    // const fd = new FormData();
-    // fd.append("image", fufilledImage);
     axios
       .post(
         "https://88q3as42oj.execute-api.us-east-1.amazonaws.com/testPlay/GoogleVision",
@@ -90,6 +56,7 @@ function ImageUpload() {
       });
   };
 
+  // JSX format for the front-end
   return (
     <div className="imageupload">
       <header className="title">
